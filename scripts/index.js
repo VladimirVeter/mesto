e8b17c91a48bba8968a11f657841d5c876f48696
Vladimir <andreevveter@mail.ru>
2022-05-20 22:27:18 +0300

    scripte

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,28 @@ const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
 
-console.log(editButton)
-console.log(popup)
+const titleElement = document.querySelector('profile__user-name')
+const nameFieldElement = document.querySelector('popup__input-name')
 
-editButton.addEventListener('click', function () {
+//console.log(editButton)
+//console.log(popup)
+
+editButton.addEventListener('click', function() {
    popup.classList.add('popup_isOpen')
+   nameFieldElement.value = titleElement.textContent;
 })
 
-closePopupButton.addEventListener('click', function () {
-   popup.classList.add('popup__close')
+closePopupButton.addEventListener('click', function() {
+   popup.classList.remove('popup_isOpen')
+   titleElement.textContent = nameFieldElement.value;
 })
+
+
+
+
+
+
+
 
 let formElement = document.querySelector('popup__form')
 let nameInput = document.querySelector('popup__input-name')
