e076e36c7baf04c0f1ddc3968206ce9f003fed4d
Vladimir <andreevveter@mail.ru>
2022-05-21 14:03:20 +0300

    code

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,11 @@ const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
 
-const titleElement = document.querySelector('profile__user-name')
-const nameFieldElement = document.querySelector('popup__input-name')
+const titleElement = document.querySelector('.profile__user-name')
+const nameFieldElement = document.querySelector('.popup__input-name')
+
+const annotationElement = document.querySelector('.profile__annotation')
+const annotationFieldElement = document.querySelector('.popup__input-annotation')
 
 //console.log(editButton)
 //console.log(popup)
@@ -11,11 +14,13 @@ const nameFieldElement = document.querySelector('popup__input-name')
 editButton.addEventListener('click', function() {
    popup.classList.add('popup_isOpen')
    nameFieldElement.value = titleElement.textContent;
+   annotationFieldElement.value = annotationElement.textContent;
 })
 
 closePopupButton.addEventListener('click', function() {
    popup.classList.remove('popup_isOpen')
    titleElement.textContent = nameFieldElement.value;
+   annotationElement.textContent = annotationFieldElement.value;
 })
 
 
@@ -25,13 +30,18 @@ closePopupButton.addEventListener('click', function() {
 
 
 
-let formElement = document.querySelector('popup__form')
-let nameInput = document.querySelector('popup__input-name')
-let jobInput = document.querySelector('popup__input-annotation')
+let formElement = document.querySelector('.popup__form')
+let nameInput = document.querySelector('.popup__input-name')
+let c = document.querySelector('.popup__input-annotation')
 
 function formSubmitHandler(evt) {
    evt.preventDefault();
 
+   editName.value = nameInput.textContent;
+   editAnnotation.value = jobInput.textContent;
+
+   nameInput.textContent = editName.value;
+   jobInput.textContent = editAnnotation.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
