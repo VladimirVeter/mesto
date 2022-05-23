ddc7d77836981ac2b06925e92f1563017c68a2e2
Vladimir <andreevveter@mail.ru>
2022-05-23 22:29:10 +0300

    fixbugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,43 @@
-const editButton = document.querySelector('.profile__edit-button')
-const popup = document.querySelector('.popup')
-const closePopupButton = document.querySelector('.popup__close')
-const saveButton = document.querySelector('.popup__button')
+const editButton = document.querySelector('.profile__edit-button')                  // вызов попап           (editButton в JS равен стилю .profile__edit-button в CSS)
+const popup = document.querySelector('.popup')                                      // форма попап           (popup в JS равен стилю .popup в CSS)
+const closePopupButton = document.querySelector('.popup__close')                    // крестик Закрыть попап (closePopupButton в JS равен стилю .popup__close в CSS)
+const saveButton = document.querySelector('.popup__button')                         // нопка Сохранить       (saveButton в JS равен стилю .popup__button в CSS)
 
-const titleElement = document.querySelector('.profile__user-name')
-const nameFieldElement = document.querySelector('.popup__input-name')
+const titleElement = document.querySelector('.profile__user-name')                  // Имя Фамилия на главной
+const nameFieldElement = document.querySelector('.popup__input-name')               // Имя Фамилия на попап
 
-const annotationElement = document.querySelector('.profile__annotation')
-const annotationFieldElement = document.querySelector('.popup__input-annotation')
+const annotationElement = document.querySelector('.profile__annotation')            // Описание юзера на главной
+const annotationFieldElement = document.querySelector('.popup__input-annotation')   // Описание юзера на попап
 
 //console.log(editButton)
 //console.log(popup)
 
-editButton.addEventListener('click', function() {
-   popup.classList.add('popup_is-open')
-   nameFieldElement.value = titleElement.textContent;
-   annotationFieldElement.value = annotationElement.textContent;
+editButton.addEventListener('click', function() {                                   // Нажми на editButton, получи три команды:
+   popup.classList.add('popup_is-open')                                             // Дабавляется класс .popup_is-open
+   nameFieldElement.value = titleElement.textContent;                               // Значение из поля ввода nameFieldElement = Значению titleElement 
+   annotationFieldElement.value = annotationElement.textContent;                    // Значение из поля ввода annotationFieldElement = Значению annotationElement
 })
 
-closePopupButton.addEventListener('click', function() {
-   popup.classList.remove('popup_is-open')
+closePopupButton.addEventListener('click', function() {                             // Нажми на editButton, получи одну команду:
+   popup.classList.remove('popup_is-open')                                          // Удалить класс .popup_is-open
    
 })
 
-saveButton.addEventListener('click', function () {
-   popup.classList.remove('popup_is-open')
-   titleElement.textContent = nameFieldElement.value;
-   annotationElement.textContent = annotationFieldElement.value;
+saveButton.addEventListener('click', function () {                                  // Нажми на saveButton, получи три команды:
+   popup.classList.remove('popup_is-open')                                          // Удалить класс .popup_is-open
+   titleElement.textContent = nameFieldElement.value;                               // Значение titleElement на главной = значению nameFieldElement на попап
+   annotationElement.textContent = annotationFieldElement.value;                    // Значение annotationElement на главной = значению annotationFieldElement на попап
 })
 
 
+let formElement = document.querySelector('.popup__form')                            // formElement в JS равен стилю .popup__form в CSS
+let nameInput = document.querySelector('.popup__input-name')                        // nameInput  в JS равен стилю .popup__input-name в CSS
+let c = document.querySelector('.popup__input-annotation')                          // С в JS равен стилю .popup__input-annotation в CSS
 
+function formSubmitHandler(evt) {                                                   // function имяФункции() { тело функции };
+   evt.preventDefault();                                                            // Эта строчка отменяет стандартную отправку формы.
 
-
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__input-name')
-let c = document.querySelector('.popup__input-annotation')
-
-function formSubmitHandler(evt) {
-   evt.preventDefault();
-
-   editName.value = nameInput.textContent;
+   editName.value = nameInput.textContent;                                          // 
    editAnnotation.value = jobInput.textContent;
 
    nameInput.textContent = editName.value;
