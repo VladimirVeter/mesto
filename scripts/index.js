79b7fd2399d031bc6da0bd260f518e3fa1d39bc5
Vladimir <andreevveter@mail.ru>
2022-05-23 21:42:12 +0300

    pp

--- scripts/index.js
+++ scripts/index.js
@@ -13,18 +13,18 @@ const annotationFieldElement = document.querySelector('.popup__input-annotation'
 //console.log(popup)
 
 editButton.addEventListener('click', function() {
-   popup.classList.add('popup__is-open')
+   popup.classList.add('popup_is-open')
    nameFieldElement.value = titleElement.textContent;
    annotationFieldElement.value = annotationElement.textContent;
 })
 
 closePopupButton.addEventListener('click', function() {
-   popup.classList.remove('popup__is-open')
+   popup.classList.remove('popup_is-open')
    
 })
 
 saveButton.addEventListener('click', function () {
-   popup.classList.remove('popup__is-open')
+   popup.classList.remove('popup_is-open')
    titleElement.textContent = nameFieldElement.value;
    annotationElement.textContent = annotationFieldElement.value;
 })
