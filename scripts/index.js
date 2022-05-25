caa7fb86e8f6019fe700d52a0a9c650ef2b78efb
Vladimir <Vladimirveter@mail.ru>
2022-05-25 11:01:17 +0300

    fixbugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,31 @@
-const editButton = document.querySelector('.profile__edit-button')                  // вызов попап           (editButton в JS равен стилю .profile__edit-button в CSS)
-const popup = document.querySelector('.popup')                                      // форма попап           (popup в JS равен стилю .popup в CSS)
-const closePopupButton = document.querySelector('.popup__close')                    // крестик Закрыть попап (closePopupButton в JS равен стилю .popup__close в CSS)
-const saveButton = document.querySelector('.popup__button')                         // нопка Сохранить       (saveButton в JS равен стилю .popup__button в CSS)
+const editButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
+const closePopupButton = document.querySelector('.popup__close')
 
-const userName = document.querySelector('.profile__user-name')                      // Имя Фамилия на главной
-const newUserName = document.querySelector('.popup__input-name')                    // Новые Имя Фамилия на попап
+const userName = document.querySelector('.profile__user-name')
+const userAnnotation = document.querySelector('.profile__annotation')
 
-const userAnnotation = document.querySelector('.profile__annotation')               // Описание юзера на главной
-const newUserAnnotation = document.querySelector('.popup__input-annotation')        // Новое описание юзера на попап
+const newUserName = document.querySelector('.popup__input-name')
+const newUserAnnotation = document.querySelector('.popup__input-annotation')
 
-const formElement = document.querySelector('.popup__form')                          // formElement в JS равен стилю .popup__form в CSS
+const formElement = document.querySelector('.popup__form')
 
-
-
-function addNewClass() {                                                            // запуск функции addNewClass
-   popup.classList.add('popup_is-open')                                             // функция содержит добавление класса .popup_is-open
+function addNewClass() {                                                   // открыли попап
+   popup.classList.add('popup_is-open');
+   newUserName.value = userName.textContent;
+   newUserAnnotation.value = userAnnotation.textContent;
 }
-editButton.addEventListener('click', addNewClass)                                   // Нажми на editButton, получи одну команду:
+editButton.addEventListener('click', addNewClass)
 
-
-editButton.addEventListener('click', function () {                                  // Нажми на editButton, получи две команды:
-   newUserName.value = userName.textContent;                                        // Значение из поля ввода newUserName = Значению userName 
-   newUserAnnotation.value = userAnnotation.textContent;                            // Значение из поля ввода newUserAnnotation = Значению userAnnotation
-})
-
-
-function removeNewClass() {                                                         // запуск функции removeNewClass
-   popup.classList.remove('popup_is-open')                                          // функция содержит удаление класса .popup_is-open
+function removeNewClass() {                                                // закрыли попап
+   popup.classList.remove('popup_is-open');
 }
-closePopupButton.addEventListener('click', removeNewClass)                          // Нажми на closePopupButton, получи одну команду:
+closePopupButton.addEventListener('click', removeNewClass)
 
-
-saveButton.addEventListener('click', removeNewClass)                                // Нажми на saveButton, получи одну команду: removeNewClass
-
-saveButton.addEventListener('click', function () {                                  // Нажми на saveButton, получи две команды:
-   userName.textContent = newUserName.value;                                        // Значение userName на главной = значению newUserName на попап
-   userAnnotation.textContent = newUserAnnotation.value;                            // Значение userAnnotation на главной = значению newUserAnnotation на попап
-})
-
-
-
-
-
-function formSubmitHandler(evt) {                                                   // function имяФункции() { тело функции };
-   evt.preventDefault();                                                            // Эта строчка отменяет стандартную отправку формы.
+function formSubmitHandler(evt) {
+   evt.preventDefault();
+   userName.textContent = newUserName.value;
+   userAnnotation.textContent = newUserAnnotation.value;
+   popup.classList.remove('popup_is-open');
 }
-
-formElement.addEventListener('submit', formSubmitHandler);  
+formElement.addEventListener('submit', formSubmitHandler);
