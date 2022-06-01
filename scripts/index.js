0e0ce5d5e04bf5c8e536227b30e22eaa63204e92
Vladimir <Vladimirveter@mail.ru>
2022-06-01 13:18:56 +0300

    5

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ const editButton = document.querySelector('.profile__edit-button')
 const popupUser = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
 
+const popupPlace = document.querySelector('.popup_place')
+const buttonNewPlace = document.querySelector('.profile__add')
+
 const userName = document.querySelector('.profile__user-name')
 const userAnnotation = document.querySelector('.profile__annotation')
 
@@ -11,12 +14,19 @@ const newUserAnnotation = document.querySelector('.popup__input_user_annotation'
 const formElement = document.querySelector('.popup__form')
 
 
+function addNewPlace() {                                                   // открыли попап
+   popupPlace.classList.add('popup_is-open');
+   }
+buttonNewPlace.addEventListener('click', addNewPlace)
+
 function addNewClass() {                                                   // открыли попап
    popupUser.classList.add('popup_is-open');
    newUserName.value = userName.textContent;
    newUserAnnotation.value = userAnnotation.textContent;
 }
 editButton.addEventListener('click', addNewClass)
+
+
 
 function removeNewClass() {                                                // закрыли попап
    popupUser.classList.remove('popup_is-open');
