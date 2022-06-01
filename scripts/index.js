c3ee6a62c59ca4e1a349000c92f84bc4b35cfabe
Vladimir <andreevveter@mail.ru>
2022-06-01 22:00:57 +0300

    5

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+   {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+   },
+   {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+   },
+   {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+   },
+   {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+   },
+   {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+   }
+]; 
+
 const editButton = document.querySelector('.profile__edit-button')
 const popupUser = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
@@ -14,6 +41,14 @@ const newUserAnnotation = document.querySelector('.popup__input_user_annotation'
 
 const formElement = document.querySelector('.popup__form')
 
+function addNewPlace(link, name) {
+   const placeTemplate = document.querySelector('.places').content;
+   const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+
+   placeElement.querySelector('.place__photo').textContent = link;
+   placeElement.querySelector('.place__name').textContent = name;
+   
+}
 
 function addNewPlace() {                                                   // открыли попап Place
    popupPlace.classList.add('popup_is-open');
