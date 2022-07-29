22500ac80ce415d2fce2783f044468f1f27429f9
Vladimir <Vladimirveter@mail.ru>
2022-07-29 13:44:38 +0300

    end

--- scripts/index.js
+++ scripts/index.js
@@ -55,7 +55,7 @@ const newPlaceLink = document.querySelector('.popup__input_place_link')
 
 const zoomPopup = document.querySelector('.popup_zoom')
 const zoomBigPhoto = document.querySelector('.popup__zoom-photo')
-const zoomPhotoName = document.querySelector('.place__name_zoom')
+const zoomPhotoName = document.querySelector('.place__name-zoom')
 const closeZoomPopupButton = document.querySelector('.popup__close_zoom')
 
 function addNewPlaceCard(item) {
